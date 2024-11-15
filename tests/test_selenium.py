a86ca9f7c4bc10d3b9e704c481d0a615853d326b
anandsamosys1 <163386716+anandsamosys1@users.noreply.github.com>
2024-11-15 16:12:37 +0530

    Update test_selenium.py

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
 import pytest
+from selenium.webdriver.chrome.options import Options
 
 @pytest.mark.selenium
 def test_001_selenium():
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    # options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(options=options)
     driver.get("https://enrichedacademy.com/")
     assert driver.title==("Enriched Academy – Canada’s #1 Financial Literacy Resource")
     driver.quit()
